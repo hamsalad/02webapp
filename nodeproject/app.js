0140ab32379128e12da8a714ff52e0e079631290
Chris Hughes <cj_hughes99@yahoo.co.uk>
2016-08-31 10:49:44 +0000

    /Start bootstrap agency

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -1,2 +1,19 @@
-var hello = 'Hello Variables!';
-console.log(hello);
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res){
+    res.send('Hello world');
+});
+
+app.get('/routing', function(req, res){
+    res.send('Hello Routing');
+});
+
+app.listen(port, function(err){
+    console.log('The server is running on port: ' + port);
+});
